27a747ea5c09e14495635833038231d5f69fa275
Byron David <byrondavid34@gmail.com.com>
2026-04-23 19:45:55 -0600

    feat: initial project structure with native c++ support

---
+++ android/app/src/main/kotlin/com/equipo5/rentasplaya/rentas_playa_cpp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.equipo5.rentasplaya.rentas_playa_cpp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ android/app/src/main/kotlin/com/example/rentas_playa_cpp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.rentas_playa_cpp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
